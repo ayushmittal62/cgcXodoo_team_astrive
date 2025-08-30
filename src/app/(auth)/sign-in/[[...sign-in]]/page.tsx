e9f30572e7c7c4ea7cdb0c6b029dafb677d356d3
dexxeth <anshgup.9450@gmail.com>
2025-08-30 14:17:12 +0530

    feat(ui): added sign-in page and mini-navbar

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignInPage } from '@/components/sign-in-flow-1'
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div className='flex w-full h-screen justify-center items-center'>
+      <SignInPage />
+    </div>
+  )
+}
+
+export default SignIn
---
+++ src/app/attendee/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Attendee = () => {
+  return (
+    <div>Attendee</div>
+  )
+}
+
+export default Attendee
